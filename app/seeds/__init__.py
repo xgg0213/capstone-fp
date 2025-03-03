f9434e7e499bc93ef6f917db6ec22d52a747196c
xgg0213 <m.eringan@gmail.com>
2025-03-02 22:54:28 -0800

    seed file sequence update

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,13 +1,12 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .orders import seed_orders, undo_orders
-from .portfolios import seed_portfolios, undo_portfolios
-from .transactions import seed_transactions, undo_transactions
-from .watchlists import seed_watchlists, undo_watchlists
 from .symbols import seed_symbols, undo_symbols
-# from .symbol_prices import seed_symbol_prices, undo_symbol_prices
 from .symbol_prices import seed_symbol_prices, undo_symbol_prices
+from .watchlists import seed_watchlists, undo_watchlists
 from .watchlist_symbols import seed_watchlist_symbols, undo_watchlist_symbols
+from .portfolios import seed_portfolios, undo_portfolios
+from .orders import seed_orders, undo_orders
+from .transactions import seed_transactions, undo_transactions
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
@@ -32,22 +31,22 @@ def seed():
     seed_users()
     seed_symbols()
     seed_symbol_prices()
-    seed_orders()
-    seed_portfolios()
-    seed_transactions()
     seed_watchlists()
-    seed_watchlist_symbols()  # Seed symbols after watchlists
+    seed_watchlist_symbols()
+    seed_portfolios()
+    seed_orders()
+    seed_transactions()
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    # Undo in reverse order of dependencies
-    undo_watchlist_symbols()  # Clear symbols before watchlists
-    undo_watchlists()
+    # The order matters here - need to undo child tables before parent tables
     undo_transactions()
-    undo_portfolios()
     undo_orders()
+    undo_portfolios()
+    undo_watchlist_symbols()
+    undo_watchlists()
     undo_symbol_prices()
     undo_symbols()
     undo_users()
