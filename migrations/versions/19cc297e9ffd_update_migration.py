ca526e11707228820958af95b419aaeba1fa00c5
xgg0213 <m.eringan@gmail.com>
2025-03-02 23:52:23 -0800

    update migrations

--- migrations/versions/19cc297e9ffd_update_migration.py
+++ migrations/versions/19cc297e9ffd_update_migration.py
@@ -1,8 +1,8 @@
 """update migration
 
-Revision ID: a665c0792eb8
+Revision ID: 19cc297e9ffd
 Revises: 
-Create Date: 2025-03-02 22:59:52.002049
+Create Date: 2025-03-02 23:51:22.089581
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a665c0792eb8'
+revision = '19cc297e9ffd'
 down_revision = None
 branch_labels = None
 depends_on = None
